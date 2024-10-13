1a069fbbe46d505b447d5cf7b83a1e9a1fd44de8
Yerushalmi, Batel <batelcohen96@gmail.com>
2024-10-13 14:06:19 +0300

    update app.start_play() function

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-def welcome():
-    user_name = input("Hi, what is your name? ")
-    print(f"Hi {user_name} and welcome to the World of Games: The Epic Journey")
+import currency_roulette_game
+import guess_game
+import memory_game
+
 
 def start_play():
-    print("Ple choose a game to play:")
+    print("Please choose a game to play:")
     print("1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back.")
     print("2. Guess Game - guess a number and see if you chose like the computer.")
     print("3. Currency Roulette - try and guess the value of a random amount of USD in ILS.")
@@ -18,9 +19,17 @@ def start_play():
     while True:
         difficulty = input("Now enter the difficulty level between 1 and 5: ")
         if difficulty.isdigit() and 1 <= int(difficulty) <= 5:
+            difficulty = int(difficulty)
             print(f"You have selected difficulty level {difficulty}.")
             break
         else:
             print("Invalid input. Please enter a difficulty level between 1 and 5.")
 
     print("We are about to start the game, prepare ;)")
+
+    if game_choice == '1':
+        memory_game.play(difficulty)
+    elif game_choice == '2':
+        guess_game.play(difficulty)
+    elif game_choice == '3':
+        currency_roulette_game.play(difficulty)
